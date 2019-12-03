c7c53a13c28c8ad571a1376611f758db7b71f08f
Alexa <alavalle@luc.edu>
2019-12-03 12:17:43 -0600

    image debug 1

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Text, View, ScrollView, Header, TouchableOpacity } from 'react-native';
+import { Text, View, Image, ScrollView, Header, TouchableOpacity } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import {styles} from '../Styles';
 
@@ -7,7 +7,7 @@ export default class Home extends Component {
     render(){
         return (
                 <View>
-                <Image source={{uri: 'COMP322logo.png'}} style={{width: 40, height: 40}} />
+                <Image source={{uri: 'COMP322logo.png'}} style={styles.image} />
                 <TouchableOpacity
                 style={styles.button}
                 onPress={() => Actions.CategoriesPage()}
