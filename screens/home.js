dbb0f3623743aadc219e4eebde0366dea30f205a
Alexa <alavalle@luc.edu>
2019-12-02 20:30:14 -0600

    another code edit

--- screens/home.js
+++ screens/home.js
@@ -1,30 +1,13 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, Image, View, ScrollView, Header, TouchableOpacity } from 'react-native';
+import { Text, View, ScrollView, Header, TouchableOpacity } from 'react-native';
+import {Actions} from 'react-native-router-flux';
+import {styles} from '../Styles';
 
-export default class CategoriesPage extends Component {
+export default class Home extends Component {
   render(){
     return (
       <Image source={{uri: 'COMP322logo.png'}} style={{width: 40, height: 40}} />
     );
   }
 };
-
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#CCCCFF',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    button:{
-      margin:20,
-      padding:10,
-      backgroundColor:"#F1E7E5",
-      borderRadius: 10,
-      width: 350,
-      borderColor: "#FEBA95",
-      borderWidth: 2,
-      alignItems: "center",
-      justifyContent: "center"
-  }
+  
