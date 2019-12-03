c0b25b93d4c02c0987a75b07d4f462cb715683c7
Alexa <alavalle@luc.edu>
2019-12-02 20:40:42 -0600

    no message

--- screens/Home.js
+++ screens/Home.js
@@ -4,17 +4,34 @@ import {Actions} from 'react-native-router-flux';
 import {styles} from '../Styles';
 
 export default class Home extends Component {
-    render (){
-        return(
-            <View style={styles.container}>
-                <Text>Home Page</Text>
-                <TouchableOpacity 
-                    style={styles.button}
-                    onPress={() => Actions.categories()}>
-                    <Text style={styles.text}>See Categories</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
-  };
+  render(){
+    return (
+      <Image source={{uri: 'COMP322logo.png'}} style={{width: 40, height: 40}} />
+      <TouchableOpacity 
+            style={styles.button}
+            onPress={() => Actions.CategoriesPage()}
+            >
+              <Text style={styles.text}>See Categories</Text>
+          </TouchableOpacity>
+    );
+  }
+};
   
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#CCCCFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button:{
+    margin:20,
+    padding:10,
+    backgroundColor:"#F1E7E5",
+    borderRadius: 10,
+    width: 350,
+    borderColor: "#FEBA95",
+    borderWidth: 2,
+    alignItems: "center",
+    justifyContent: "center"
+}
