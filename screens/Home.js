b090de88e0be4f2e33052668653b5241f7d778c5
Alexa <alavalle@luc.edu>
2019-12-03 15:31:47 -0600

    router

--- screens/Home.js
+++ screens/Home.js
@@ -2,19 +2,21 @@ import React, {Component} from 'react';
 import { Text, View, Image, ScrollView, Header, TouchableOpacity } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import {styles} from './Styles';
+import Router from './Router'
 
 export default class Home extends Component {
     render(){
         return (
+                <Router/>
                 <View>
-                <Image source={{uri: 'COMP322logo.png'}} style={styles.image} />
-                <TouchableOpacity
-                style={styles.button}
-                onPress={() => Actions.categories()}
-                >
-                <Text style={styles.text}>See Categories</Text>
-                </TouchableOpacity>
+                    <Image source={{uri: 'COMP322logo.png'}} style={styles.image} />
+                    <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => Actions.categories()}
+                    >
+                    <Text style={styles.text}>See Categories</Text>
+                    </TouchableOpacity>
                 </View>
-                );
+            );
     }
 };
