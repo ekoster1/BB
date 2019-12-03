271780fd9b36817facb4d9630748f61fe9b417c2
Maha <42777088+MahaBKT@users.noreply.github.com>
2019-12-02 20:10:45 -0600

    updated header.js, might need some work

---
+++ screens/Header.js
@@ -0,0 +1,6 @@
+import { Icon } from 'react-native-elements'
+<Header
+leftComponent={{icon:'menu', color: '#fff'}}
+centerComponent={{text:'Welcome', style:{color:'#fff'} }}
+rightComponent={{icon: 'home', style:{color: '#fff'}}}
+/>
