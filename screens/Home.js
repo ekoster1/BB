6ebafcda6bec74b7ab3097819525aebc1059d37b
Alexa <alavalle@luc.edu>
2019-12-03 11:25:36 -0600

    merged categories

--- screens/Home.js
+++ screens/Home.js
@@ -4,18 +4,17 @@ import {Actions} from 'react-native-router-flux';
 import {styles} from '../Styles';
 
 export default class Home extends Component {
-  render(){
-    return (
-        <View>
-            <Image source={{uri: 'COMP322logo.png'}} style={{width: 40, height: 40}} />
-            <TouchableOpacity 
+    render(){
+        return (
+                <View>
+                <Image source={{uri: 'COMP322logo.png'}} style={{width: 40, height: 40}} />
+                <TouchableOpacity
                 style={styles.button}
                 onPress={() => Actions.CategoriesPage()}
                 >
                 <Text style={styles.text}>See Categories</Text>
-            </TouchableOpacity>
-        </View>
-    );
-  }
+                </TouchableOpacity>
+                </View>
+                );
+    }
 };
-  
