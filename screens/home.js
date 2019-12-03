7c85d056005df911a9610403e9b33057ac75a8f6
Alexa <alavalle@luc.edu>
2019-12-02 20:34:38 -0600

    Home agian

--- screens/home.js
+++ screens/home.js
@@ -7,7 +7,31 @@ export default class Home extends Component {
   render(){
     return (
       <Image source={{uri: 'COMP322logo.png'}} style={{width: 40, height: 40}} />
+      <TouchableOpacity 
+            style={styles.button}
+            onPress={() => Actions.CategoriesPage()}
+            >
+              <Text style={styles.text}>See Categories</Text>
+          </TouchableOpacity>
     );
   }
 };
   
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#CCCCFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button:{
+    margin:20,
+    padding:10,
+    backgroundColor:"#F1E7E5",
+    borderRadius: 10,
+    width: 350,
+    borderColor: "#FEBA95",
+    borderWidth: 2,
+    alignItems: "center",
+    justifyContent: "center"
+}
