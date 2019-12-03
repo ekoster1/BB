af3deac42b799ff3030ed7944bfa2aa4e18804a2
Alexa <alavalle@luc.edu>
2019-12-03 11:20:37 -0600

    fixed style

--- screens/Home.js
+++ screens/Home.js
@@ -19,22 +19,3 @@ export default class Home extends Component {
   }
 };
   
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#CCCCFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button:{
-    margin:20,
-    padding:10,
-    backgroundColor:"#F1E7E5",
-    borderRadius: 10,
-    width: 350,
-    borderColor: "#FEBA95",
-    borderWidth: 2,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
