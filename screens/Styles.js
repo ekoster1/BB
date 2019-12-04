9f7dbe43ca306a43b0e94ecdcc6fd083996b71ea
Alexa <alavalle@luc.edu>
2019-12-03 19:51:14 -0600

    image part 2

--- screens/Styles.js
+++ screens/Styles.js
@@ -23,8 +23,8 @@ const styles = StyleSheet.create({
         fontSize: 20,
     },
     image: {
-        width: 300,
-        height: 300, 
+        width: 100,
+        height: 100, 
         margin: 10,
         marginTop: 0,
         alignItems: "center",
