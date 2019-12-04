654200a537cdee61bf4b39ad25d8cecc80ea6db8
Alexa <alavalle@luc.edu>
2019-12-03 20:47:20 -0600

    no text input for now

--- screens/Home.js
+++ screens/Home.js
@@ -10,9 +10,7 @@ export default class Home extends Component {
                 <View style={styles.container}>
                     <Image source={require('../assets/COMP322logo.png')} style={styles.image} />
                     <Text style={styles.text}>Login</Text>
-                    <TextInput style={styles.textInput} placeholder='Username' onChangeText={(text) => this.setState({text})}
-                        value={this.state.text}/>
-                    <TextInput placeholder='Password'/>
+                    
                     <TouchableOpacity
                     style={styles.button}
                     onPress={() => Actions.categories()}
