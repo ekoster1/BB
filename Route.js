0ecea52dc06f71c841ce737d20761d63b935ec5b
emilykoster <ekoster1@luc.ed>
2019-12-03 19:34:41 -0600

    updated Route

--- Route.js
+++ Route.js
@@ -28,30 +28,32 @@ import React, { Component } from 'react';
 import { View, Platform} from 'react-native';
 import {Actions, Router, Scene, Stack} from 'react-native-router-flux';
 
-import Playlist from './Playlist';
-import WatchVideo from './Video';
-import CategoriesPage from './Categories';
-import LoginPage from './LoginPage';
-import Home from './Home';
-import EyeShape from './categories/EyeShape';
-import FaceShape from './categories/FaceShape';
-import SkinType from './categories/SkinType';
-import SkinTone from './categories/SkinTone';
+import Playlist from './screens/Playlist';
+import WatchVideo from './screens/Video';
+import CategoriesPage from './screens/Categories';
+import LoginPage from './screens/LoginPage';
+import Home from './screens/Home';
+import EyeShape from './screens/categories/EyeShape';
+import FaceShape from './screens/categories/FaceShape';
+import SkinType from './screens/categories/SkinType';
+import SkinTone from './screens/categories/SkinTone';
 
 export default class Routes extends Component {
   render(){
-    <Router>
-        <Stack key="root">
-            <Scene key="home" component={Home} title="Home" initial/>
-            <Scene key="categories" component={CategoriesPage} title="Facial Categories"/>
-            <Scene key="EyeShape" component={EyeShape} title="Eye Shape"/>
-            <Scene key="FaceShape" component={FaceShape} title="Face Shape"/>
-            <Scene key="SkinTone" component={SkinTone} title="Skin Tone"/>
-            <Scene key="SkinType" component={SkinType} title="Skin Type"/>
-            <Scene key="playlist" component={Playlist} title="Playlist"/>
-            <Scene key="video" component={WatchVideo} title="Video Player"/>
-            <Scene key="login" component={LoginPage} title="Login"/>
-        </Stack>
-    </Router>
+      return(
+        <Router>
+            <Stack key="root">
+                <Scene key="home" component={Home} title="Home" initial/>
+                <Scene key="categories" component={CategoriesPage} title="Facial Categories"/>
+                <Scene key="EyeShape" component={EyeShape} title="Eye Shape"/>
+                <Scene key="FaceShape" component={FaceShape} title="Face Shape"/>
+                <Scene key="SkinTone" component={SkinTone} title="Skin Tone"/>
+                <Scene key="SkinType" component={SkinType} title="Skin Type"/>
+                <Scene key="playlist" component={Playlist} title="Playlist"/>
+                <Scene key="video" component={WatchVideo} title="Video Player"/>
+                <Scene key="login" component={LoginPage} title="Login"/>
+            </Stack>
+        </Router>
+      )
   }
 }
