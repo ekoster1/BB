9a41953ba024dcd2906a87ab31b3fe132d169629
Alexa <alavalle@luc.edu>
2019-12-03 20:54:22 -0600

    styling 4

--- screens/Styles.js
+++ screens/Styles.js
@@ -45,7 +45,7 @@ const styles = StyleSheet.create({
         top:150,
     },
     facialImage: {
-        width: 200,
+        width: 100,
         height: 40, 
         marginRight: 25,
         marginTop: 0,
