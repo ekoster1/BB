434f4a0734d330f24c184debcaa8fbd1656eec84
emilykoster <ekoster1@luc.ed>
2019-12-14 10:59:02 -0600

    skintone playlists added

--- screens/SkinTone.js
+++ screens/SkinTone.js
@@ -5,27 +5,31 @@ import {styles} from './Styles';
 
 export default class SkinTone extends Component {
   render(){
+      const goToPale = () => Actions.playlist({id : "PL6sCTSXxmmq9eNoM64UQJSAG62LdYSbsN"})
+      const goToDark = () => Actions.playlist({id : "PL6sCTSXxmmq8Equ7ZwyFJz4McQLculiRp"})
+      const goToTan = () => Actions.playlist({id : "PL6sCTSXxmmq-aTwosuY8epoQRSYg9ZIAA"})
+
     return (
       <View style={styles.container}>
         <ScrollView>
           <View></View>
           <TouchableOpacity 
             style={styles.button}
-            onPress={() => Actions.CoolTone()}
+            onPress={goToPale}
             >
-              <Text style={styles.text}>Cool Tone</Text>
+              <Text style={styles.text}>Pale/Light</Text>
           </TouchableOpacity>
           <TouchableOpacity 
             style={styles.button}
-            onPress={() => Actions.NeutralTone()}
+            onPress={goToTan}
             >
-              <Text style={styles.text}>Neutral Tone</Text>
+              <Text style={styles.text}>Tan</Text>
           </TouchableOpacity>
           <TouchableOpacity 
             style={styles.button}
-            onPress={() => Actions.WarmTone()}
+            onPress={goToDark}
             >
-              <Text style={styles.text}>Warm Tone</Text>
+              <Text style={styles.text}>Dark</Text>
           </TouchableOpacity>
         </ScrollView>
       </View>
