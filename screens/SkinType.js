8081b5d8544dfc5d7f7ed0b6fa2f4acb2a171f33
emilykoster <ekoster1@luc.ed>
2019-12-13 21:47:23 -0600

    added playlists for skin type

--- screens/SkinType.js
+++ screens/SkinType.js
@@ -5,27 +5,32 @@ import {styles} from './Styles';
 
 export default class SkinType extends Component {
   render(){
+      const goToDry = () => Actions.playlist({id : "PL6sCTSXxmmq_D7f2gH-bIJAiMDeEPrT4J"})
+      const goToOily = () => Actions.playlist({id : "PL6sCTSXxmmq9AOiRZAVwcfEH33l14XCEb"})
+      const goToCombo = () => Actions.playlist({id : "PL6sCTSXxmmq8SouFOjD6a3CsEI-W9uTIe"})
     return (
       <View style={styles.container}>
         <ScrollView>
+            <Text style={styles.categoriesText}>Skin Type</Text>
+  
           <View></View>
           <TouchableOpacity 
             style={styles.button}
-            onPress={() => Actions.DrySkin()}
+            onPress={goToDry}
             >
               <Text style={styles.text}>Dry Skin</Text>
           </TouchableOpacity>
           <TouchableOpacity 
             style={styles.button}
-            onPress={() => Actions.OilySkin()}
+            onPress={goToOily}
             >
               <Text style={styles.text}>Oily Skin</Text>
           </TouchableOpacity>
           <TouchableOpacity 
             style={styles.button}
-            onPress={() => Actions.ComboSkin()}
+            onPress={goToCombo}
             >
-              <Text style={styles.text}>Combo Skin</Text>
+              <Text style={styles.text}>Combination Skin</Text>
           </TouchableOpacity>
         </ScrollView>
       </View>
